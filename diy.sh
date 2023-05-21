a53372450be26fe46de4fc4ae6b25087bacd7842
marchen12 <130596644+marchen12@users.noreply.github.com>
2023-05-21 18:21:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,4 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
